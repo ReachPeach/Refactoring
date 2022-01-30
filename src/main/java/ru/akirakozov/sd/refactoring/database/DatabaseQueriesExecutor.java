899a837336d60f0bf0296b02f9a2a9ec486f4dfe
ReachPeach <maxim.busyuk@gmail.com>
2022-01-30 18:35:59 +0300

    {refactor}[codedrop] Added database abstraction

---
+++ src/main/java/ru/akirakozov/sd/refactoring/database/DatabaseQueriesExecutor.java
@@ -0,0 +1,26 @@
+package ru.akirakozov.sd.refactoring.database;
+
+import java.sql.*;
+
+public class DatabaseQueriesExecutor implements AutoCloseable {
+    private final Statement statement;
+    private final Connection connection;
+
+    public DatabaseQueriesExecutor(final String dataBaseUrl) throws SQLException {
+        this.connection = DriverManager.getConnection(dataBaseUrl);
+        this.statement = connection.createStatement();
+    }
+
+    public void executeUpdate(final String update) throws SQLException {
+        statement.executeUpdate(update);
+    }
+
+    public ResultSet executeQuery(final String query) throws SQLException {
+        return statement.executeQuery(query);
+    }
+
+    public void close() throws SQLException {
+        statement.close();
+        connection.close();
+    }
+}
